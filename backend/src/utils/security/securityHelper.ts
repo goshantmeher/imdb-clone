cf45769203b6483721be665e317a672762542e6d
Goshant Meher <goshantmeher@gmail.com>
2025-04-05 19:43:15 +0530

    update security middleware: configure CORS settings for specific origin and methods

--- backend/src/utils/security/securityHelper.ts
+++ backend/src/utils/security/securityHelper.ts
@@ -4,7 +4,13 @@ import cors from 'cors';
 
 export const initSecurity = (app: Application) => {
    //    app.use(express.json());
-   app.use(cors());
+   app.use(
+      cors({
+         origin: 'http://localhost:5173',
+         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+         credentials: true,
+      }),
+   );
 
    app.use(bodyParser.json({ limit: '2kb' }));
    app.use(bodyParser.urlencoded({ extended: true, limit: '1kb' }));
